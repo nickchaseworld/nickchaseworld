519845d95e5248ae890ccc03dbd3e4a70c9d23df
Orlando Kenny <orlandokenny@Orlandos-MacBook-Pro.local>
2021-08-09 17:59:46 -0400

    update

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
 import reportWebVitals from './reportWebVitals';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
-/*using react router we set a constant equal to whatever component
-we would like to render*/
-const routing = (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home}/>  
-      <Route exact path="/*" component={Home}/>    
-    </Switch>
-  </Router>
-)
 
-/*the code below reads the path and renders component on a conditional basis.
-i.e. /home throws two different components at different places...*/
-ReactDOM.render(routing, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
